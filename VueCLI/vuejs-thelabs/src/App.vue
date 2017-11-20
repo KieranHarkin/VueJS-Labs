af6ff954d901876c2110d1a6334f648af187921c
= <kieranharkin@gmail.com>
2017-11-20 00:22:14 +0000

    Input binding

--- VueCLI/vuejs-thelabs/src/App.vue
+++ VueCLI/vuejs-thelabs/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div>
-    <!-- Doesn't destroy component -->
-    <keep-alive> 
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <button @click="component = 'form-one'">Show Form One</button>
-    <button @click="component = 'form-two'">Show Form Two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 
 <script>
 
-import formOne from './components/formOne.vue';
-import formTwo from './components/formTwo.vue';
+import addBlog from './components/addBlog.vue';
 
 export default {
   components: {
-    'form-one': formOne,
-    'form-two': formTwo
+    'add-blog' : addBlog
   },
   data() {
     return{
-      component: 'form-one'
     }
   },
   methods: {
@@ -32,5 +24,8 @@ export default {
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
 </style>

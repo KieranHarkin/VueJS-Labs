b366ea0ffc3682a41295b80a456f173b5af37448
= <kieranharkin@gmail.com>
2017-11-23 13:15:20 +0000

    Using Mixins

--- VueCLI/vuejs-thelabs/src/components/listBlogs.vue
+++ VueCLI/vuejs-thelabs/src/components/listBlogs.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="show-blogs">
-    <h1>All Blog Articles</h1>
-    <input type="text" v-model="search" placeholder="Search Blogs">
-    <div v-for="blog in filteredBlogs" class="single-blog">
-      <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
-      <article>
-        {{blog.body | snippet}}
-      </article>
-    </div>
+      <h1>List Blog Titles</h1>
+      <input type="text" v-model="search" placeholder="search blogs">
+      <div v-for="blog in filteredBlogs" class="single-blog">
+          <h2 v-rainbow>
+              {{blog.title | to-uppercase}}
+          </h2>
+      </div>
   </div>
 </template>
 
@@ -30,7 +29,6 @@ export default {
       this.blogs = data.body.slice(0, 10);      
     });
   },
-
   filters: {
     // 'to-uppercase': function(value) {
     //   return value.toUpperCase();
@@ -46,7 +44,7 @@ export default {
       }
     }
   },
-  mixins: [searchMixin]
+    mixins: [searchMixin]
 }
 </script>
 
@@ -66,3 +64,4 @@ export default {
 
 
 </style>
+
